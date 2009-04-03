dff681228d8b4bec6b3810f297998b0b5be64ce2
kipp <kipp@7114b090-0338-4b8b-9344-e0c13efb9f03>
2009-04-03 00:14:16 +0000

    - update libtool to the version on my system - snapshot the output of aclocal into acinclude.m4 for use on less   well-endowed computers - comment out the -I /usr/share/autoconf-archive option to aclocal in   00init.sh
    git-svn-id: https://geco.phys.columbia.edu/projects/svn/sphradiometer/trunk@741 7114b090-0338-4b8b-9344-e0c13efb9f03

--- 00init.sh
+++ 00init.sh
@@ -6,7 +6,8 @@ read -p "Press CTRL-C to abort, or RETURN to continue... " INPUT
 
 # Get it done
 {
-	{ echo "running aclocal (please ignore \"underquoted\" warnings)..." ; aclocal -I /usr/share/autoconf-archive ; } &&
+	#{ echo "running aclocal (please ignore \"underquoted\" warnings)..." ; aclocal -I /usr/share/autoconf-archive ; } &&
+	{ echo "running aclocal (please ignore \"underquoted\" warnings)..." ; aclocal ; } &&
 	{ echo "running autoheader..." ; autoheader ; } &&
 	{ echo "running automake..." ; automake -a -c ; } &&
 	{ echo "running autoconf..." ; autoconf ; } &&
