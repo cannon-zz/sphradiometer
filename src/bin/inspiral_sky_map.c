5c94924e4ec010596cb271ebf146594a3510c9fe
Tsutsui Takuya <tsutsui@resceu-gw2.s.u-tokyo.ac.jp>
2019-03-28 17:28:59 +0900

    inspiral_sky_map.c: fixing for incompatible pointer problem

--- src/bin/inspiral_sky_map.c
+++ src/bin/inspiral_sky_map.c
@@ -38,6 +38,8 @@
 #include <sphradiometer/correlator.h>
 #include <diagnostics.h>
 #include <instruments.h>
+#include <chealpix.h>
+#include <hdf5.h>
 
 #include <lal/Date.h>
 #include <lal/DetResponse.h>
@@ -444,7 +446,7 @@ static struct correlator_network_plan_fd *correlator_network_plan_mult_by_projec
 
 int main(int argc, char *argv[])
 {
-	struct options *options;
+	//struct options *options;
 	COMPLEX8TimeSeries *series[2];
 	struct correlator_network_baselines *baselines;
 	double *tseries;
@@ -460,6 +462,89 @@ int main(int argc, char *argv[])
 	double gmst;
 	const LALDetector det[2];
 	int i;
+	hid_t file_id, dataset_id, dataspace_id, filespace_id; //
+	herr_t status;
+	double *dset_data;
+	int rank;
+	hsize_t dims[2];
+
+
+	// FIXME: set tseries and initial_time
+//	file_id = H5Fopen("output.h5", H5F_ACC_RDWR, H5P_DEFAULT);	// open an existing file
+//	dataset_id = H5Dopen(file_id, "/dset", H5P_DEFAULT);		// open an existing dataset
+//	filespace_id = H5Dget_space(dataset_id);			// read the dataset
+//	rank = H5Sget_simple_extent_ndims(filespace_id);		//
+//	status = H5Sget_simple_extent_dims(filespace_id, dims, NULL);	//
+//	dset_data = calloc(sizeof(double), dims[0]*dims[1]);		//
+//	status = H5Dread(dataset_id, H5T_NAIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, dset_data); //
+	
+
+	complex float tseries_h1[] = {-3.983366012573242 -I*1.126609563827515 ,
+		-4.169796466827393 -I*0.1484837979078293 ,
+		-3.969766616821289+ I*0.7086409330368042 ,
+		-3.590789794921875+ I*1.213011264801025 ,
+		-3.466731309890747+ I*1.413019061088562 ,
+		-3.683328866958618+ I*1.755069017410278 ,
+		-3.960615873336792+ I*2.538711786270142 ,
+		-3.926766633987427+ I*3.765216112136841 ,
+		-3.297438383102417+ I*5.179051399230957 ,
+		-1.979390144348145+ I*6.397994041442871 ,
+		-0.1174830719828606+ I*7.072439193725586 ,
+		1.977809309959412+ I*6.977499485015869 ,
+		3.828668355941772+ I*6.035830974578857 ,
+		5.097399234771729+ I*4.576348781585693 ,
+		5.724094867706299+ I*2.90929913520813 ,
+		5.729362964630127+ I*1.364532113075256 ,
+		5.399999618530273+ I*0.1394371688365936 ,
+		4.840004920959473 -I*0.8327525854110718 ,
+		4.231722354888916 -I*1.482487440109253 ,
+		3.69499659538269 -I*1.961324572563171 ,
+		3.169871807098389 -I*2.357835292816162 ,
+	};
+	complex float tseries_l1[] = {4.783010959625244+ I*4.272365093231201 ,
+		5.034692287445068+ I*3.548733711242676 ,
+		5.366576194763184+ I*3.200068950653076 ,
+		5.998232364654541+ I*3.035331010818481 ,
+		7.141907691955566+ I*2.736834764480591 ,
+		8.686773300170898+ I*1.842572093009949 ,
+		10.26880168914795+ I*0.006665350869297981 ,
+		11.27248287200928 -I*2.897961139678955 ,
+		11.00472545623779 -I*6.521040439605713 ,
+		9.039669036865234 -I*10.08332538604736 ,
+		5.528451919555664 -I*12.57289409637451 ,
+		1.317213177680969 -I*13.27334213256836 ,
+		-2.55349063873291 -I*12.10455894470215 ,
+		-5.085619449615479 -I*9.672429084777832 ,
+		-6.066431045532227 -I*7.106531620025635 ,
+		-6.081847667694092 -I*5.159073829650879 ,
+		-5.780312061309814 -I*4.009140491485596 ,
+		-5.821097850799561 -I*3.332641124725342 ,
+		-6.196267127990723 -I*2.49298095703125 ,
+		-6.42664098739624 -I*1.259080767631531 ,
+		-6.144642353057861+ I*0.1295482814311981
+	};
+	complex float tseries_v1[] = {-0.7310531735420227+ I*1.292859077453613 ,
+		-0.5984863638877869+ I*1.589023351669312 ,
+		-0.3703035116195679+ I*1.957797408103943 ,
+		0.06488069146871567+ I*2.389910221099854 ,
+		0.8175532817840576+ I*2.72331714630127 ,
+		1.821033835411072+ I*2.719309568405151 ,
+		2.848188877105713+ I*2.27576470375061 ,
+		3.702071189880371+ I*1.421364903450012 ,
+		4.21214771270752+ I*0.230946958065033 ,
+		4.220407485961914 -I*1.115646958351135 ,
+		3.713864088058472 -I*2.333185911178589 ,
+		2.857001543045044 -I*3.230406999588013 ,
+		1.830233216285706 -I*3.75080394744873 ,
+		0.7930911183357239 -I*3.883560180664062 ,
+		-0.08061070740222931 -I*3.709914207458496 ,
+		-0.7251806855201721 -I*3.417925357818604 ,
+		-1.229905009269714 -I*3.130890607833862 ,
+		-1.694764614105225 -I*2.844826221466064 ,
+		-2.152580738067627 -I*2.520911693572998 ,
+		-2.606910943984985 -I*2.119481086730957 ,
+		-3.038182497024536 -I*1.595876097679138
+	};
 
 
 	/*
@@ -467,7 +552,13 @@ int main(int argc, char *argv[])
 	 */
 
 
-	options = command_line_parse(argc, argv);
+	//options = command_line_parse(argc, argv);
+	struct options *options = command_line_options_new();
+	options->integration_duration = 21 * 0.00048828125;
+	options->instruments->instruments[0] = instrument_from_LALDetector(XLALDetectorPrefixToLALDetector("H1"));
+	options->instruments->instruments[1] = instrument_from_LALDetector(XLALDetectorPrefixToLALDetector("L1"));
+	//options->instruments[2] = instrument_from_LALDetector(XLALDetectorPrefixToLALDetector("V1"));
+	time_series_length = 21; //
 
 
 	/*
@@ -475,6 +566,7 @@ int main(int argc, char *argv[])
 	 */
 
 
+	/*
 	series[0] = get_complex8series_from_cache(options->data1_cache_name, options->data1_channel_name, options->analysis_start, options->analysis_duration);
 	series[1] = get_complex8series_from_cache(options->data2_cache_name, options->data2_channel_name, options->analysis_start, options->analysis_duration);
 
@@ -486,6 +578,18 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "sample rate mismatch\n");
 		exit(1);
 	}
+	*/
+
+	LIGOTimeGPS epoch1, epoch2;
+	XLALINT8NSToGPS(&epoch1, 0);
+	XLALINT8NSToGPS(&epoch2, 1);
+	series[0] = XLALCreateCOMPLEX8TimeSeries("H1", &epoch1, 0, 0.00048828125, &lalSecondUnit, 21);
+	free(series[0]->data->data);
+	series[0]->data->data = tseries_h1;
+
+	series[1] = XLALCreateCOMPLEX8TimeSeries("L1", &epoch2, 0, 0.00048828125, &lalSecondUnit, 21);
+	free(series[1]->data->data);
+	series[1]->data->data = tseries_l1;
 
 	window = XLALCreateRectangularREAL8Window(options->integration_duration / series[0]->deltaT);
 	if(window->data->length * series[0]->deltaT != options->integration_duration) {
@@ -517,6 +621,7 @@ int main(int argc, char *argv[])
 	 */
 
 
+	fprintf(stderr, "%g", time_series_length);
 	fprintf(stderr, "starting integration\n");
 	gettimeofday(&t_start, NULL);
 	for(start_sample = 0; start_sample + time_series_length <= series[0]->data->length; start_sample += time_series_length / 2) {
@@ -524,7 +629,7 @@ int main(int argc, char *argv[])
 		 * Extract the data to integrate.
 		 */
 
-
+		
 		fprintf(stderr, "\t[%.3f s, %.3f s)\n", start_sample * series[0]->deltaT, (start_sample + time_series_length) * series[0]->deltaT);
 		for(k = 0; k < instrument_array_len(options->instruments); k++) {
 			memcpy(tseries, &series[k]->data->data[start_sample], time_series_length * sizeof(*tseries));
@@ -576,6 +681,10 @@ int main(int argc, char *argv[])
 	correlator_network_baselines_free(baselines);
 	sh_series_free(sky);
 	command_line_options_free(options);
+
+	status = H5Dclose(dataset_id);	// close the dataset
+	status = H5Fclose(file_id);	// close the file
+	free(dset_data);		//
 
 	return 0;
 }
