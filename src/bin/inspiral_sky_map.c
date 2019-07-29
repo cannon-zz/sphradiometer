27193b2b2afe5311e39ab88ca59dd40cb9b3e034
Tsutsui Takuya <tsutsui@resceu-gw2.s.u-tokyo.ac.jp>
2019-07-29 13:40:19 +0900

    inspiral_sky_map: Tukey window
    - remove Widow Norm because it's canceled by whitening
    - apply this window to template auto-correlation

--- src/bin/inspiral_sky_map.c
+++ src/bin/inspiral_sky_map.c
@@ -857,9 +857,10 @@ int main(int argc, char *argv[])
 	int j;
 	for(k = 0; k < instrument_array_len(options->instruments); k++){
 		REAL8Window *window = XLALCreateTukeyREAL8Window(series[k]->data->length, 0.1);
-		REAL4 WinNorm = sqrt(window->sumofsquares/window->data->length);
-		for(j = 0; j < (int) window->data->length; j++)
-			series[k]->data->data[j] *= window->data->data[j] / WinNorm;
+		for(j = 0; j < (int) window->data->length; j++){
+			series[k]->data->data[j] *= window->data->data[j];
+			nseries[k]->data[j] *= window->data->data[j];
+		}
 	}
 
 
