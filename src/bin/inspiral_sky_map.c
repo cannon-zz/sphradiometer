0c8b660343518f31c207ef4e55982a07f773f266
Kipp Cannon <kipp.cannon@ligo.org>
2020-05-14 01:38:01 -0700

    inspiral_sky_map:  fix (I think) bad #if

--- src/bin/inspiral_sky_map.c
+++ src/bin/inspiral_sky_map.c
@@ -1030,7 +1030,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "can't save positive plan\n");
 		exit(1);
 	}
-#endif
 #else
 	/* NOTE: series[0]->data->length in precalculated objects must be
 	 * equivalnt to series[0]->data->length in this code */
