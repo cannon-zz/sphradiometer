7f8e93dfb7c9b85e8daea2653648ce5a3f035f38
Kipp Cannon <kipp.cannon@ligo.org>
2021-12-08 14:24:17 +0900

    update a comment in healpy_validate.py
    - healpix does not support m < 0 (real-valued functions on the sphere, only)

--- test/healpy_validate.py
+++ test/healpy_validate.py
@@ -6,7 +6,7 @@ from healpy.sphtfunc import Alm
 
 series = sh_series_new(64, 0)
 for l in range(series.l_max + 1):
-	# FIXME:  m < 0 is messed up
+	# NOTE:  m < 0 not supported by healpix
 	for m in range(0, l + 1):
 		if sh_series_lmoffset(series, l, m) != Alm.getidx(series.l_max, l, m):
 			print("for (l, m) = (%d, %d): sphradiometer's index s %d, healpy's index is %d" % (l, m, sh_series_lmoffset(series, l, m), Alm.getidx(series.l_max, l, m)))
