daf98c72df1b7199b1526fe4af7ea341ec974d54
Kipp Cannon <kipp.cannon@ligo.org>
2022-08-23 15:46:43 +0900

    try again to port to openblas
    - what was in here before was probably working OK, but I think this gets the
      #include's and libraries set up properly, for real this time, on both
      redhat and debian.  it's a stupid mess, but it's HPC stuff, so I think
      there's nothing that can be done, I think they like living their lives
      this way.

--- src/lib/deconvolution.c
+++ src/lib/deconvolution.c
@@ -27,8 +27,8 @@
  */
 
 
-#include <cblas.h>
 #include <complex.h>
+#include <lapacke.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
@@ -125,7 +125,7 @@ static struct SVD *SVD_new(int m, int n, const complex double *A)
 	memcpy(new->A_new, A, m * n * sizeof(*new->A_new));
 	transpose(m, n, &new->A_new);
 
-	zgesdd_(&job, &m, &n, new->A_new, &lda, new->S, new->U, &ldu, new->VT, &ldvt, work, &lwork, rwork, iwork, &info);
+	LAPACK_zgesdd(&job, &m, &n, new->A_new, &lda, new->S, new->U, &ldu, new->VT, &ldvt, work, &lwork, rwork, iwork, &info);
 
 	lwork = work[0];
 	free(work);
@@ -136,7 +136,7 @@ static struct SVD *SVD_new(int m, int n, const complex double *A)
 		goto done;
 	}
 
-	zgesdd_(&job, &m, &n, new->A_new, &lda, new->S, new->U, &ldu, new->VT, &ldvt, work, &lwork, rwork, iwork, &info);
+	LAPACK_zgesdd(&job, &m, &n, new->A_new, &lda, new->S, new->U, &ldu, new->VT, &ldvt, work, &lwork, rwork, iwork, &info);
 
 	transpose(n, m, &new->A_new);
 	transpose(n, m, &new->U);
