5145eb5ade2a4fbb20ff2d3c331fdd4c38f28d17
Kipp Cannon <kipp@g.ecc.u-tokyo.ac.jp>
2025-08-07 15:33:04 +0900

    bin/inspiral_sky_map.c
    - fix copyright notice.  this was written by myself and Tsutsui-san

--- src/bin/inspiral_sky_map.c
+++ src/bin/inspiral_sky_map.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009  Kipp Cannon, Chad Hanna
+ * Copyright (C) 2012--2025  Kipp Cannon, Tsutsui Takuya
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
